4b0beeadbaf10c8544c806c3e9ff5aa7ff9dfb5e
Jakub Korečko <sakon@bitbucket.org>
2017-11-02 18:52:26 +0100

    Updated template, bootstrap files
    * bootstrap update to 4.0.0-beta.2
    * js files locate from the local folder
    * navbar fix - edited to sticky-top
    * carousel fix - not using images in the background, changed to block view
    * contact form update - textarea added
    * add footer - scroll to top added
    * few changes to font styling

---
+++ js/main.js
@@ -0,0 +1,1 @@
+function myMap(){var o=document.getElementById("map"),n={center:new google.maps.LatLng(51.5,-.2),zoom:10};new google.maps.Map(o,n)}!function(o){o(document).ready(function(){o("#top").click(function(){return o("body,html").animate({scrollTop:0},1e3),!1})})}(jQuery);
